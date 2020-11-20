5cb1a352560d0c8340b7d5325c7a975eaabc78ac
李铭昕 <715557344@qq.com>
2020-11-20 18:47:43 +0800

    完善 Auth

--- src/Auth.php
+++ src/Auth.php
@@ -18,4 +18,70 @@ class Auth
     const FORBIDDEN = 1;
 
     const TOKEN_NOT_FOUND = 2;
+
+    const FAILED = 3;
+
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var int
+     */
+    protected $project;
+
+    /**
+     * @var string
+     */
+    protected $route;
+
+    /**
+     * @var int
+     */
+    protected $result;
+
+    /**
+     * @var string
+     */
+    protected $message;
+
+    public function __construct(int $id, int $project, string $route, int $result, string $message = '')
+    {
+        $this->id = $id;
+        $this->project = $project;
+        $this->route = $route;
+        $this->result = $result;
+        $this->message = $message;
+    }
+
+    public function isSuccess(): bool
+    {
+        return $this->result === static::SUCCESS;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getProject(): int
+    {
+        return $this->project;
+    }
+
+    public function getRoute(): string
+    {
+        return $this->route;
+    }
+
+    public function getResult(): int
+    {
+        return $this->result;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
 }
