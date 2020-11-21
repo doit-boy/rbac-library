c29120c83dec6854fa1eb6d6bf76ed446684c435
李铭昕 <715557344@qq.com>
2020-11-21 12:40:59 +0800

    Added testClientInvokerInClientFactory.

--- tests/Cases/ClientFactoryTest.php
+++ tests/Cases/ClientFactoryTest.php
@@ -13,6 +13,8 @@ namespace HyperfTest\Cases;
 
 use DoitBoy\RBAC\Client;
 use DoitBoy\RBAC\ClientFactory;
+use DoitBoy\RBAC\Contract\HandlerInvokerInterface;
+use GuzzleHttp\HandlerStack;
 use Hyperf\Config\Config;
 use Hyperf\Contract\ConfigInterface;
 use Mockery;
@@ -45,5 +47,43 @@ class ClientFactoryTest extends AbstractTestCase
         $client = $factory($container);
         $this->assertInstanceOf(Client::class, $client);
         $this->assertSame($options, $client->getOptions());
+    }
+
+    public function testClientInvokerInClientFactory()
+    {
+        $container = Mockery::mock(ContainerInterface::class);
+        $container->shouldReceive('get')->with(ConfigInterface::class)->andReturn(new Config([
+            'rbac' => [
+                'options' => $options = [
+                    'base_uri' => 'http://rbac_api:8000',
+                    'handler' => new class() implements HandlerInvokerInterface {
+                        public function __invoke(ContainerInterface $container): HandlerStack
+                        {
+                            return new HandlerStack(new class() {
+                                public function isMock()
+                                {
+                                    return true;
+                                }
+
+                                public function __invoke()
+                                {
+                                }
+                            });
+                        }
+                    },
+                    'timeout' => 2,
+                ],
+            ],
+        ]));
+
+        $factory = new ClientFactory();
+        $client = $factory($container);
+        $handler = $client->getOptions()['handler'];
+        $this->assertInstanceOf(HandlerStack::class, $handler);
+        $ref = new \ReflectionClass($handler);
+        $property = $ref->getProperty('handler');
+        $property->setAccessible(true);
+        $handler = $property->getValue($handler);
+        $this->assertTrue($handler->isMock());
     }
 }
